8a5d2c90e796da89edacf3da490533177a238d4a
jbassin <jbassin@bu.edu>
2017-10-28 16:49:58 -0400

    Added a bunch of functions

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from sentence_parser import parse
+
+parser = parse.Parse()
+parser.get_subtree('Those who find ugly meanings in beautiful things are corrupt without being charming.')
--- sentence_parser/parse.py
+++ sentence_parser/parse.py
@@ -6,3 +6,23 @@ class Parse:
     def __init__(self):
         self.parser = spacy.load('en')
         return
+
+    def pos_tag(self, sentence):
+        corpus = self.parser(sentence)
+        for word in corpus:
+            print(word.lemma_, word.tag_, word.dep_, word.head.lemma_)
+        return
+
+    def pos_adj_stripper(self, sentence):
+        corpus = self.parser(sentence)
+        for word in corpus:
+            if word.pos_ is 'ADJ' and word.tag_ is not 'PRP$':
+                print(word.text, word.tag_)
+        return
+
+    def get_subtree(self, sentence):
+        corpus = self.parser(sentence)
+        subtrees = list([w for w in corpus if w.head is w][0].lefts)
+        for subtree in subtrees:
+            for descendant in subtree.subtree:
+                print(descendant)
