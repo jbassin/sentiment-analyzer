bf9814f7a46bfe391d8e2d90913f17cedc70971a
michaeldjaballah <madjabal@bu.edu>
2017-10-29 03:38:19 -0400

    Added polarity

--- sentence_parser/sentiment.py
+++ sentence_parser/sentiment.py
@@ -11,7 +11,53 @@ class Sentiment:
     def polarity(self, list):
         polarlist = [self.get_polarity(dictionary) for dictionary in list if self.get_polarity(dictionary) != 0]
         abslist = [abs(n) for n in polarlist]
-        return sum(polarlist)/len(polarlist), sum(abslist)/len(abslist)
+        return [sum(polarlist)/len(polarlist), sum(abslist)/len(abslist)]
 
-    def test(self):
-        print('polarity(["hate", "spook"])) should return', self.polarity(["hate", "spook"]))
+    def polarity_(self, num):
+        indifferent = 20
+        weak = 40
+        mild = 70
+        decisive = 20
+        somewhatd = 37
+        neutral = 62
+        somewhatm = 80
+
+        if num[0] > 0:
+            pol = 'positive'
+            if abs(num[0]) >= mild:
+                mod = 'strong'
+            elif abs(num[0]) >= weak:
+                mod = 'mild'
+            elif abs(num[0]) >= indifferent:
+                mod = 'weak'
+            else:
+                pol = ''
+                mod = 'indifferent'
+
+        elif num[0] < 0:
+            pol = 'negative'
+            if abs(num[0]) >= mild:
+                mod = 'strong'
+            elif abs(num[0]) >= weak:
+                mod = 'mild'
+            elif abs(num[0]) >= indifferent:
+                mod = 'weak'
+            else:
+                pol = ''
+                mod = 'indifferent'
+        else:
+            mod = 'very'
+            pol = 'indifferent'
+
+        if num[1] >= somewhatm:
+            mag = 'mixed'
+        elif num[1] >= neutral:
+            mag = 'somewhat mixed'
+        elif num[1] >= somewhatd:
+            mag = 'neither decisive nor mixed'
+        elif num[1] > decisive:
+            mag = 'somewhat decisive'
+        else:
+            mag = 'decisive'
+        return mag + mod + pol
+
