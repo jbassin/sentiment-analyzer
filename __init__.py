c75fc1d97de8532efc4abfd96617d13d206580d2
michaeldjaballah <madjabal@bu.edu>
2017-10-29 04:17:45 -0400

    Added averages and fixed indexes

--- __init__.py
+++ __init__.py
@@ -12,11 +12,11 @@ class TotalParse:
     def check_keyword(self, query, count):
         biglist = self.twitter_calc.get_text(query, count)
         bigscorelist = [self.sentiment_calc.polarity(self.parser.get_significant_words(i)) for i in biglist]
-        bigpolar = sum([i[0] for i in bigscorelist])
-        bigmag = sum([i[0] for i in bigscorelist])
+        bigpolar = sum([i[0] for i in bigscorelist])/len([i[0] for i in bigscorelist])
+        bigmag = sum([i[1] for i in bigscorelist])/len([i[1] for i in bigscorelist])
         complete = [bigpolar, bigmag]
         return self.sentiment_calc.polarity_(complete)
 
 
 parser = TotalParse()
-parser.check_keyword('Clinton', 100)
+print(parser.check_keyword('death', 100))
