0af6e7e1334c3f097356f2fd488f6b909e960af5
michaeldjaballah <madjabal@bu.edu>
2017-10-29 02:34:13 -0400

    New file with wordblob, changes to sentiment

--- sentence_parser/sentiment.py
+++ sentence_parser/sentiment.py
@@ -7,3 +7,11 @@ class Sentiment:
 
     def get_polarity(self, dictionary):
         return dictionary['negate'] * int(TextBlob(dictionary['word']).sentiment.polarity * 100.0)
+
+    def polarity(self, list):
+        polarlist = [self.get_polarity(dictionary) for dictionary in list]
+        abslist = [abs(n) for n in polarlist]
+        return sum(polarlist)/len(polarlist), sum(abslist)/len(abslist)
+
+    def test(self):
+        print('polarity(["hate", "spook"])) should return', self.polarity(["hate", "spook"]))
